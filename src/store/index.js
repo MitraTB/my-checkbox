ad438da24bf8b24822aed5af9df567b7ea648deb
unknown <mitra.tabatabaei.asl@gmail.com>
2021-01-30 00:22:28 +0330

    displaying data according to the checked items

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    chekboxesItems :[
+      {
+      id:1,
+      "name" : "First",
+      "subitems" : ["first group one", "first group two" , "first group three"],
+      "icon" : "folder-plus"
+      },
+      {
+        "id":2,
+        "name" : "second",
+        "subitems" : ["second group one" , "second group two"],
+        "icon": "person-square"
+      },
+      {
+        "id" : 3,
+        "name" : "third",
+        "subitems" : ["third group one","third group two", "third group three","third group four"],
+        "icon":"grid3x3-gap"
+      }
+  ]
   },
   mutations: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container bg-dark text-white">
+      <div class="checkBoxes">
+        <div class="checkboxesList" v-for="item in chekboxesItems" :key="item.id">
+          <input type="checkbox"  :value="item.subitems" v-model="itemInfos"/>
+          <label class="ml-2"><b-icon :icon="item.icon"></b-icon></label>
+        </div>
+      </div>
+      <hr>
+      <div class="display-subItems mt-3 mr-5">
+          <div v-for="itemInfo in itemInfos" :key="itemInfo.id">
+            <ul>
+            <li style="list-style:none;" class="mt-3" v-for="element in itemInfo" :key="element.id">{{element}}</li>
+            </ul>
+          </div>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+     itemInfos :[]
+    }
+  },
+  computed:{
+    chekboxesItems(){
+      let result = this.$store.state.chekboxesItems;
+      console.log(result);
+      return result;
+    },
   }
-}
+ 
+  }
 </script>
+<style scoped>
+.container {
+  width:30%;
+  border: black solid 2px;
+}
+.checkBoxes{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+li:hover{
+  background-color: gray;
+}
+
+
+
+</style>
